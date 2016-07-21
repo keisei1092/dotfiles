51859d64d278dd7a148fe50cb79b3b0723da3307
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-07-21 16:35:16 +0900

    zsh自分で打ってください…

--- ubuntu_16_04_setup_1.sh
+++ ubuntu_16_04_setup_1.sh
@@ -5,10 +5,3 @@
 sudo apt-get update -y
 sudo apt-get install -y build-essential
 sudo apt-get install -y git
-
-# zsh =====
-# http://dev.classmethod.jp/tool/zsh-prezto/
-
-sudo apt-get install -y zsh
-
-zsh # => please type keys to configure zsh.
--- ubuntu_16_04_setup_3.sh
+++ ubuntu_16_04_setup_3.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# zsh =====
-# http://dev.classmethod.jp/tool/zsh-prezto/
-
-sudo apt-get install -y zsh
-
-zsh # => please type keys to configure zsh.
-
-# zprezto =====
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
-
-chsh -s /bin/zsh
-
 # Vim =====
 
 sudo apt-get install -y vim-gnome
