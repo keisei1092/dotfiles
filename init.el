05f23ad09e96b881a4762e24ae9f302b130ba93c
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-10-13 07:13:36 +0900

    update

--- init.el
+++ init.el
@@ -1,3 +1,52 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (keyboard-translate ?\C-h ?\C-?)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(global-linum-mode 1)
+(set-default 'truncate-lines t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/.cask/25.1/elpa/color-theme-solarized-20160626.743/")
+(set-frame-parameter nil 'background-mode 'dark)
+(load-theme 'solarized t)
+
+(when (and (>= emacs-major-version 24) (not (null window-system)))
+  (let* ((font-family "SF Mono")
+         (font-size 12)
+         (font-height (* font-size 9))
+         (jp-font-family "Hiragino Sans W6"))
+    (set-face-attribute 'default nil :family font-family :height font-height)
+    (let ((name (frame-parameter nil 'font))
+          (jp-font-spec (font-spec :family jp-font-family))
+          (jp-characters '(katakana-jisx0201
+                           cp932-2-byte
+                           japanese-jisx0212
+                           japanese-jisx0213-2
+                           japanese-jisx0213.2004-1))
+          (font-spec (font-spec :family font-family))
+          (characters '((?\u00A0 . ?\u00FF)    ; Latin-1
+                        (?\u0100 . ?\u017F)    ; Latin Extended-A
+                        (?\u0180 . ?\u024F)    ; Latin Extended-B
+                        (?\u0250 . ?\u02AF)    ; IPA Extensions
+                        (?\u0370 . ?\u03FF)))) ; Greek and Coptic
+      (dolist (jp-character jp-characters)
+        (set-fontset-font name jp-character jp-font-spec))
+      (dolist (character characters)
+        (set-fontset-font name character font-spec))
+      (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.2)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (##))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
