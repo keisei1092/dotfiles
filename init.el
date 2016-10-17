8aaae65ec73b21d4206811f73f76d2812a0482f5
keisei_1092 <keisei_1092@yahoo.co.jp>
2016-10-17 09:32:57 +0900

    update settings

--- init.el
+++ init.el
@@ -5,21 +5,40 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 (keyboard-translate ?\C-h ?\C-?)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (global-linum-mode 1)
+(electric-pair-mode 1)
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+			      (interactive)
+			      (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+        	      	      (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/.cask/24.5/elpa/color-theme-solarized-20160626.743/")
+)
+(global-set-key (kbd "S-C-b") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-f") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-n") 'shrink-window)
+(global-set-key (kbd "S-C-p") 'enlarge-window)
 (set-default 'truncate-lines t)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/.cask/25.1/elpa/color-theme-solarized-20160626.743/")
-(set-frame-parameter nil 'background-mode 'dark)
+(set-frame-parameter nil 'background-mode 'light)
 (load-theme 'solarized t)
 
 (when (and (>= emacs-major-version 24) (not (null window-system)))
   (let* ((font-family "SF Mono")
          (font-size 12)
          (font-height (* font-size 9))
-         (jp-font-family "Hiragino Sans W6"))
-    (set-face-attribute 'default nil :family font-family :height font-height)
+         (jp-font-family "Hiragino Sans"))
+    (set-face-attribute 'default nil :family font-family :height font-height :weight 'semibold)
     (let ((name (frame-parameter nil 'font))
           (jp-font-spec (font-spec :family jp-font-family))
           (jp-characters '(katakana-jisx0201
@@ -38,6 +57,7 @@
       (dolist (character characters)
         (set-fontset-font name character font-spec))
       (add-to-list 'face-font-rescale-alist (cons jp-font-family 1.2)))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,3 +70,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
